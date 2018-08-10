0e9a288d2c9a5d617188ce79fb0358f0e871b32d
Marco-Malacara <marco.malacara7@gmail.com>
2018-08-09 21:27:28 -0500

    push for submit

--- CustomListTest/CustomListTest/Program.cs
+++ CustomListTest/CustomListTest/Program.cs
@@ -10,18 +10,6 @@ namespace CustomListTest
     {
         static void Main(string[] args)
         {
-            CustomList<string> listOne = new CustomList<string> { "Marco", "Aly", "Trent", "Mike", "OtherMike" };
-            CustomList<string> listTwo = new CustomList<string> { "Marco", "Aly", "Trenten", "Mike", "OtherMike" };
-            CustomList<string> newList = new CustomList<string>();
-
-            //Act
-            newList = (listOne - listTwo);
-
-            foreach (string item in newList)
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadLine();
-            }
+        }
     }
 }
